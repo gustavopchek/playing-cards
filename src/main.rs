d62eb041fe7dbeb842a6d6194c24d8bdd34dc1fd
Gustavo Pchek <ghpk88@gmail.com>
2024-10-31 13:39:36 -0300

    adding shuffling

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,49 @@
+use rand::{thread_rng, seq::SliceRandom};
+
 #[derive(Debug)]
 struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    // List of 'suits' - 'diamonds', 'clubs'
-    // List of 'values' - 'ace', 'two', 'three'
+impl Deck {
+    // return type annotation
+    // same as fn new() -> Deck
+    fn new() -> Self {
+        // List of 'suits' - 'diamonds', 'clubs'
+        // List of 'values' - 'ace', 'two', 'three'
 
-    // Double nested for loops
+        // Double nested for loops
 
-    let suits = ["Hearts", "Spades", "Clubs", "Diamonds"];
-    let values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+        let suits = ["Hearts", "Spades", "Clubs", "Diamonds"];
+        let values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
 
-    // make it mutable
-    let mut cards = vec![];
+        // make it mutable
+        let mut cards = vec![];
 
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", value, suit);
-            cards.push(card);
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
         }
+
+        // same as { cards: cards }
+        // without 
+        Deck { cards }
     }
 
-    // same as { cards: cards }
-    let deck = Deck { cards };
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+
+        self.cards.shuffle(&mut rng);
+    }
+}
+
+
+fn main() {
+    let mut deck = Deck::new();
+
+    deck.shuffle();
     // print each element on a new line
     println!("Here is the deck: {:#?}", deck);
 }
